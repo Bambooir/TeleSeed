3095c8e5ddf508d259b8ed40e042a1508179ca92
iman daneshi <emandaneshikohan@gmail.com>
2015-12-10 13:56:06 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -4,7 +4,7 @@ local function run(msg)
 local bot_id = our_id -- your bot id
    -- like local bot_id = 1234567
     if msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my group.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	end
 end
