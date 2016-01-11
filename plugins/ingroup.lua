682605be1f7c36d4c8a24467e4b51a6a9a46e68f
Rondoozle <devmwheeler@live.com>
2016-01-10 22:35:12 -0500

    "Group Help Does not show in realm"

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1187,7 +1187,7 @@ local function run(msg, matches)
      end
    end
     if matches[1] == 'help' then
-      if not is_momod(msg) and not is_realm(msg) then
+      if not is_momod(msg) or is_realm(msg) then
         return
       end
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] Used /help")
