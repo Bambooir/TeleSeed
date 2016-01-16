cb66cfd9dfb03c3801c28e68431aeeb4ac5a0607
buzz3rs <mmeqaby21@yahoo.com>
2016-01-16 10:38:18 +0300

    fixed some spell errors

--- plugins/info.lua
+++ plugins/info.lua
@@ -21,7 +21,7 @@ local function action_by_reply(extra, success, result)
   local chat = 'chat#id'..msg.to.id
   local data = load_data(_config.moderation.data)
   if data[tostring('admins')][tostring(user_id)] then
-    who = 'Admim'
+    who = 'Admin'
   elseif data[tostring(msg.to.id)]['moderators'][tostring(user_id)] then
     who = 'Moderator'
   elseif data[tostring(msg.to.id)]['set_owner'] == tostring(user_id) then
