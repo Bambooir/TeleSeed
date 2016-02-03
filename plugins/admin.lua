a13c89b447e0f1f33f3ebd4006c6a4a0587e7dfe
iman daneshi <emandaneshikohan@gmail.com>
2016-02-03 21:03:43 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -18,7 +18,93 @@ local function parsed_url(link)
   local parsed_path = URL.parse_path(parsed_link.path)
   return parsed_path[2]
 end
-
+local function get_contact_list_callback (cb_extra, success, result)
+  local text = " "
+  for k,v in pairs(result) do
+    if v.print_name and v.id and v.phone then
+      text = text..string.gsub(v.print_name ,  "_" , " ").." ["..v.id.."] = "..v.phone.."\n"
+    end
+  end
+  local file = io.open("contact_list.txt", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  send_document("user#id"..cb_extra.target,"contact_list.txt", ok_cb, false)--.txt format
+  local file = io.open("contact_list.json", "w")
+  file:write(json:encode_pretty(result))
+  file:flush()
+  file:close()
+  send_document("user#id"..cb_extra.target,"contact_list.json", ok_cb, false)--json format
+end
+local function user_info_callback(cb_extra, success, result)
+  result.access_hash = nil
+  result.flags = nil
+  result.phone = nil
+  if result.username then
+    result.username = '@'..result.username
+  end
+  result.print_name = result.print_name:gsub("_","")
+  local text = serpent.block(result, {comment=false})
+  text = text:gsub("[{}]", "")
+  text = text:gsub('"', "")
+  text = text:gsub(",","")
+  if cb_extra.msg.to.type == "chat" then
+    send_large_msg("chat#id"..cb_extra.msg.to.id, text)
+  else
+    send_large_msg("user#id"..cb_extra.msg.to.id, text)
+  end
+end
+local function get_dialog_list_callback(cb_extra, success, result)
+  local text = ""
+  for k,v in pairs(result) do
+    if v.peer then
+      if v.peer.type == "chat" then
+        text = text.."group{"..v.peer.title.."}["..v.peer.id.."]("..v.peer.members_num..")"
+      else
+        if v.peer.print_name and v.peer.id then
+          text = text.."user{"..v.peer.print_name.."}["..v.peer.id.."]"
+        end
+        if v.peer.username then
+          text = text.."("..v.peer.username..")"
+        end
+        if v.peer.phone then
+          text = text.."'"..v.peer.phone.."'"
+        end
+      end
+    end
+    if v.message then
+      text = text..'\nlast msg >\nmsg id = '..v.message.id
+      if v.message.text then
+        text = text .. "\n text = "..v.message.text
+      end
+      if v.message.action then
+        text = text.."\n"..serpent.block(v.message.action, {comment=false})
+      end
+      if v.message.from then
+        if v.message.from.print_name then
+          text = text.."\n From > \n"..string.gsub(v.message.from.print_name, "_"," ").."["..v.message.from.id.."]"
+        end
+        if v.message.from.username then
+          text = text.."( "..v.message.from.username.." )"
+        end
+        if v.message.from.phone then
+          text = text.."' "..v.message.from.phone.." '"
+        end
+      end
+    end
+    text = text.."\n\n"
+  end
+  local file = io.open("dialog_list.txt", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  send_document("user#id"..cb_extra.target,"dialog_list.txt", ok_cb, false)--.txt format
+  local file = io.open("dialog_list.json", "w")
+  file:write(json:encode_pretty(result))
+  file:flush()
+  file:close()
+  send_document("user#id"..cb_extra.target,"dialog_list.json", ok_cb, false)--json format
+end
 local function run(msg,matches)
     local data = load_data(_config.moderation.data)
     local receiver = get_receiver(msg)
@@ -63,22 +149,43 @@ local function run(msg,matches)
     	unblock_user("user#id"..matches[2],ok_cb,false)
     	return "User unblocked"
     end
-    if matches[1] == "import" then
+    if matches[1] == "import" then--join by group link
     	local hash = parsed_url(matches[2])
     	import_chat_link(hash,ok_cb,false)
+    end
+    if matches[1] == "contactlist" then
+      get_contact_list(get_contact_list_callback, {target = msg.from.id})
+      return "I've sent contact list with both json and text format to your private"
+    end
+    if matches[1] == "delcontact" then
+      del_contact("user#id"..matches[2],ok_cb,false)
+      return "User "..matches[2].." removed from contact list"
+    end
+    if matches[1] == "dialoglist" then
+      get_dialog_list(get_dialog_list_callback, {target = msg.from.id})
+      return "I've sent dialog list with both json and text format to your private"
+    end
+    if matches[1] == "whois" then
+      user_info("user#id"..matches[2],user_info_callback,{msg=msg})
     end
     return
 end
 return {
   patterns = {
-    "^[!/](pm) (%d+) (.*)$",
-    "^[!/](import) (.*)$",
-    "^[!/](unblock) (%d+)$",
-    "^[!/](block) (%d+)$",
+	"^[!/](pm) (%d+) (.*)$",
+	"^[!/](import) (.*)$",
+	"^[!/](unblock) (%d+)$",
+	"^[!/](block) (%d+)$",
 	"^[!/](markread) (on)$",
 	"^[!/](markread) (off)$",
-    "^[!/](setbotphoto)$",
-	"%[(photo)%]"
+	"^[!/](setbotphoto)$",
+	"%[(photo)%]",
+	"^[!/](contactlist)$",
+	"^[!/](dialoglist)$",
+	"^[!/](delcontact) (%d+)$",
+	"^[!/](whois) (%d+)$"
   },
   run = run,
 }
+--By @imandaneshi :)
+--https://github.com/SEEDTEAM/TeleSeed/blob/master/plugins/admin.lua
