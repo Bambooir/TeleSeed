ed318a6c916dec89e45812fba84c2eeb44315261
BossTG <ghasem4954@gmail.com>
2016-02-14 14:09:53 +0330

    Create lock_join.lua

---
+++ plugins/lock_join.lua
@@ -0,0 +1,19 @@
+local function run (msg, matches)
+local data = load_data(_config.moderation.data)
+   if matches[1] == 'chat_add_user_link' then
+        local user_id = msg.from.id
+        if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+        if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
+      kick_user(user_id, msg.to.id)
+    end
+   end
+end   
+ end
+ end
+return {
+  patterns = {
+    "^!!tgservice (chat_add_user_link)$"
+  },
+  run = run
+}
