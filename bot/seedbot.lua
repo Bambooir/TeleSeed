5c25dd50cba4edcdc08cedaffc77c5074e4366d3
sina7skk <hacker.7sk.33@gmail.com>
2016-03-09 20:06:01 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {140529465,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {114934518},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
