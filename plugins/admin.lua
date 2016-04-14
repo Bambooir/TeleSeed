334f8f7b851dec4372de103e61dc13eaee8cb5f3
Shahab <safaeishahab@yahoo.com>
2016-04-15 01:37:25 +0430

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -303,6 +303,72 @@ local function pre_process(msg)
   end
   return msg
 end
+-- Checks if bot was disabled on specific chat
+local function is_channel_disabled( receiver )
+	if not _config.disabled_channels then
+		return false
+	end
+
+	if _config.disabled_channels[receiver] == nil then
+		return false
+	end
+
+  return _config.disabled_channels[receiver]
+end
+
+local function enable_channel(receiver)
+	if not _config.disabled_channels then
+		_config.disabled_channels = {}
+	end
+
+	if _config.disabled_channels[receiver] == nil then
+		return 'Robot is Online'
+	end
+	
+	_config.disabled_channels[receiver] = false
+
+	save_config()
+	return "Bot is On"
+end
+
+local function disable_channel( receiver )
+	if not _config.disabled_channels then
+		_config.disabled_channels = {}
+	end
+	
+	_config.disabled_channels[receiver] = true
+
+	save_config()
+	return "Bot is Off"
+end
+
+local function pre_process(msg)
+	local receiver = get_receiver(msg)
+
+	if is_admin(msg) then
+	  if msg.text == "[#/!]bot on" then
+	    enable_channel(receiver)
+	  end
+	end
+
+  if is_channel_disabled(receiver) then
+  	msg.text = ""
+  end
+
+	return msg
+end
+
+local function run(msg, matches)
+	local receiver = get_receiver(msg)
+	-- Enable a channel
+	if matches[1] == 'on' then
+		return enable_channel(receiver)
+	end
+	-- Disable a channel
+	if matches[1] == 'off' then
+		return disable_channel(receiver)
+	end
+end
 
 return {
   patterns = {
@@ -323,6 +389,8 @@ return {
 	"^[#/!](updateid)$",
 	"^[#/!](addlog)$",
 	"^[#/!](remlog)$",
+        "^[#/!]bot? (on)", 
+	"^[#/!]bot? (off)",
 	"%[(photo)%]",
   },
   run = run,
