b2a973cf81f8e24dd51ad1ddf623e10e37b82d02
anupritaisno1 <www.anuprita804@gmail.com>
2016-11-12 04:20:51 +0000

    another one!

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 while true; do
 echo updating teleseed >> /dev/null 2>&1
-cd ~/TeleSeed >> /dev/null 2>&1
+cd ~/Clannadbot >> /dev/null 2>&1
 git pull >> /dev/null 2>&1
 git checkout supergroups >> /dev/null 2>&1
 sleep 7200 >> /dev/null 2>&1
